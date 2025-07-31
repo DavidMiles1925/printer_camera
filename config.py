0e9527215cbc52b454e79875a1c3b01d4a647b19
David Miles <davidmiles1925@gmail.com>
2025-07-31 12:50:18 -0500

    add robust error handling

--- config.py
+++ config.py
@@ -80,6 +80,7 @@ DISABLE_CAMERA = False
 REMOTE_HOST = "192.168.1.161"
 PI_USERNAME = "pidgey"
 WIN_USERNAME = "David"
+
 REMOTE_PATH_PHOTO = f"/home/{PI_USERNAME}/printer_camera/photos/"
 REMOTE_PATH_VIDEO = f"/home/{PI_USERNAME}/printer_camera/recordings/"
 LOCAL_PATH = f"C:/Users/{WIN_USERNAME}/Downloads"
--- pull_files.py
+++ pull_files.py
@@ -1,9 +1,14 @@
 # This file is only to be used from a windows machine that has been paired with a Pi via SSH Key
 
 import os
-from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 from time import sleep
+
+try:
+    from paramiko import SSHClient, AutoAddPolicy
+except:
+    print("\nparmiko not installed\n")
+    pass
 
 from config import REMOTE_HOST, REMOTE_PATH_PHOTO, REMOTE_PATH_VIDEO, LOCAL_PATH, PI_USERNAME
 
@@ -12,7 +17,7 @@ def progress(filename, size, sent):
     print(f"\rTransferring {filename}: {percent:.1f}% ({sent}/{size} bytes)", end="")
 
 def fetch_files():
-    try:
+    
         print(f"Connecting to {REMOTE_HOST}...")
 
         ssh = SSHClient()
@@ -25,12 +30,55 @@ def fetch_files():
             scp.get(REMOTE_PATH_VIDEO, LOCAL_PATH, recursive=True)
 
         ssh.close()
-        print(f"Done! Files copied to {LOCAL_PATH}")
-    except Exception as e:
-        print(e)
-        sleep(5)
+        print(f"\n\nDone! Files copied to {LOCAL_PATH}")
+        sleep(2)
+
 
 if __name__ == "__main__":
     print("program started")
     sleep(1)
-    fetch_files()
+
+    try:
+        fetch_files()
+
+    except NameError as e:
+        print(e.__class__)
+        print(e)
+        print("\n\n")
+        print("HOW TO SOLVE:")
+        print("If 'paramiko not installed' is displayed above, run this command:\n\n")
+        print("pip install paramiko\n\n")
+        print("Press any key...")
+        input()
+
+
+    except FileNotFoundError as e:
+        print(e.__class__)
+        print(e)
+        print("\n\n")
+        print("HOW TO SOLVE:")
+        print("Please configure WIN_USERNAME in the config.py file.")
+        print("Press any key...")
+        input()
+
+    except TimeoutError as e:
+        print(e.__class__)
+        print(e)
+        print("\n\n")
+        print("HOW TO SOLVE:")
+        print("Please check REMOTE_HOST in the config.py file.")
+        print("Please check that the device is turned on.")
+        print("Press any key...")
+        input()
+
+    except Exception as e:
+        print(e)
+        print(e.__class__)
+
+        if str(e) == "Authentication failed.":
+            print("\n\n")
+            print("HOW TO SOLVE:")
+            print("Please configure PI_USERNAME in the config.py file.")
+
+        print("Press any key...")
+        input()
